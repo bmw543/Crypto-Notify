a9a75487b625f87a4da55c4b5db77053bbaf20bb
Thomas Neubert <57598094+bmw543@users.noreply.github.com>
2021-02-04 21:44:22 -0500

    Update Crypto_Price.py

--- Crypto Notify/Crypto_Price.py
+++ Crypto Notify/Crypto_Price.py
@@ -6,11 +6,11 @@ import os
 
 # coins
 
-coins = np.array([["Stellar", "https://www.coindesk.com/price/stellar", 0.30, 0.30],
-                  ["Algorand", "https://www.coindesk.com/price/algorand", 0.70, 0.76],
-                  ["Bitcoin Cash", "https://www.coindesk.com/price/bitcoin-cash", 400, 480],
-                  ["Bitcoin", "https://www.coindesk.com/price/bitcoin", 34000, 39000],
-                  ["Litecoin", "https://www.coindesk.com/price/litecoin", 140, 160]])
+coins = np.array([[True, "Stellar", "https://www.coindesk.com/price/stellar", 0.30, 0.30],
+                  [True, "Algorand", "https://www.coindesk.com/price/algorand", 0.70, 0.76],
+                  [True, "Bitcoin Cash", "https://www.coindesk.com/price/bitcoin-cash", 400, 480],
+                  [True, "Bitcoin", "https://www.coindesk.com/price/bitcoin", 34000, 39000],
+                  [True, "Litecoin", "https://www.coindesk.com/price/litecoin", 140, 160]])
 
 # paths
 
@@ -32,11 +32,12 @@ class Coin():
         self.price = float(results.find(class_="price-large").text[1:].replace(",", ""))
 
 for coin in coins:
-    coin = Coin(coin[0], coin[1], coin[2], coin[3])
-    coin.get_price()
-    notification = ToastNotifier()
-    if (coin.price < coin.low_price):
-        notification.show_toast(coin.name, coin.name + " has fallen below $" + str(coin.low_price), duration=20, icon_path=icons_path + coin.name + ".ico", threaded = True)
-    if (coin.price > coin.high_price):
-        notification.show_toast(coin.name, coin.name + " has risen above $" + str(coin.high_price), duration=20, icon_path=icons_path + coin.name + ".ico", threaded = True)
+    if(coin[0]):
+      coin = Coin(coin[1], coin[2], coin[3], coin[4])
+      coin.get_price()
+      notification = ToastNotifier()
+      if (coin.price < coin.low_price):
+          notification.show_toast(coin.name, coin.name + " has fallen below $" + str(coin.low_price), duration=20, icon_path=icons_path + coin.name + ".ico", threaded = True)
+      if (coin.price > coin.high_price):
+          notification.show_toast(coin.name, coin.name + " has risen above $" + str(coin.high_price), duration=20, icon_path=icons_path + coin.name + ".ico", threaded = True)
 
